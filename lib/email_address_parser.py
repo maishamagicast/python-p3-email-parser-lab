e14c5d71fb25a130704a05911132e144a541a780
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-06-27 16:52:03 +0300

    1st commit

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,11 @@
 # your code goes here!
+import re
+class EmailAddressParser:
+    def __init__(self,email):
+        self.email=email
+        
+    def parse(self):
+        parts=self.email.replace(',',' ').split()
+        email_pattern = re.compile(r'^[\w\.-]+@[\w\.-]+\.\w+$')
+        sorted_emails=sorted(set(filter(email_pattern.fullmatch,parts)))
+        return sorted_emails
